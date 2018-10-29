e16edb5c598dc47cc3ab928e3536415532baacce
donaldma <donald.ma@hotmail.ca>
2018-10-29 12:17:24 -0700

    fetch every 10s

--- client/src/containers/Map.js
+++ client/src/containers/Map.js
@@ -43,7 +43,7 @@ class Map extends Component {
   componentDidMount() {
     window.addEventListener('resize', this.resize)
     this.resize()
-    setInterval(this.props.getBuses, 20000)
+    setInterval(this.props.getBuses, 10000)
   }
 
   componentWillUnmount() {
